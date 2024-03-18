c0437717bd0ba518927ef403d0567ae6c2fcc727
Melissa Stadt <84604412+mstadt@users.noreply.github.com>
2024-03-18 15:59:05 -0400

    Update modeqns_PKPD.m
    fix bug

--- modeqns_PKPD.m
+++ modeqns_PKPD.m
@@ -41,10 +41,16 @@ dydt = zeros(length(y),1);
 
 %% Peripheral blood compartment
 % d(CARTe_PB)/dt
-dydt(1) = (-K12*Vb*CARTe_PB + K21*Vt*CARTe_T)./Vb - Kel_e*CARTe_PB;
+CARTe_T2PB = K21*Vt*CARTe_T; % CARTe cell T to PB compartment
+CARTe_PB2T = K12*Vb*CARTe_PB; % CARTe cell PB to T compartment
+CARTe_PB_deg = Kel_e*CARTe_PB; % CARTe degradation in PB
+dydt(1) = (CARTe_T2PB - CARTe_PB2T)./Vb - CARTe_PB_deg;
 
 % d(CARTm_PB)/dt
-dydt(2) = (K21*Vt*CARTm_T-K12*Vb*CARTm_PB)./Vb  - Kel_m*CARTm_PB;
+CARTm_T2PB = K21*Vt*CARTm_T; % CARTm cell T to PB compartment
+CARTm_PB2T = K12*Vb*CARTm_PB; % CARTm cell PB to T compartment
+CARTm_PB_deg = Kel_m*CARTm_PB; % degradation of CARTm in PB
+dydt(2) = (CARTm_T2PB - CARTm_PB2T)./Vb  - CARTm_PB_deg;
 
 %% Tissue compartment (bone marrow or solid tumor)
 if (CARTe_T + CARTm_T) > 0
@@ -56,10 +62,12 @@ else
 end
 
 % d(CARTe_T)/dt
-dydt(3) = (K12*Vb*CARTe_PB - K21*Vt*CARTe_T)./Vt + Kexp*CARTe_T - Rm*CARTe_T;
+CARTe_exp = Kexp * CARTe_T; % CART cell expansion
+CARTe2CARTm = Rm * CARTe_T; % conversion from effector to memory T-cells
+dydt(3) = (CARTe_PB2T - CARTe_T2PB)./Vt + CARTe_exp - CARTe2CARTm;
 
 % d(CARTm_T)/dt
-dydt(4) = (K12*Vb*CARTm_PB - K21*Vt*CARTm_T)./Vt + Rm*CARTe_T;
+dydt(4) = (CARTm_PB2T - CARTm_T2PB)./Vt + CARTe2CARTm;
 
 % Cplx
 f_CART = (CARTe_T + CARTm_T) * Ag_CAR - Cplx;
